19b721f113f116b551b8163ebb02dc68bbaca27f
PavanKumar355 <pavankumar93477@gmail.com>
2024-12-25 22:45:55 +0530

    added some examples on anonymous,arrow and named functions

--- TypesOfFunctions.js
+++ TypesOfFunctions.js
@@ -15,3 +15,31 @@ let greet3 = (name)=>{
     return `Hello,${name}`
 }
 console.log(greet3('Abhi'))
+
+//named function example
+function Message(message){
+    return "welcome to javascript learning "+message
+}
+console.log( Message('Alice'))
+
+//anonymous function
+// let product=function (a,b){
+//    console.log(a*b)
+// }
+
+// function sum(a,b,fun){
+//     console.log(a+b)
+// fun(6,8)
+// }
+
+// sum(12,12,product)
+
+function quotation(quote){
+    console.log(quote+" hardwork never fails..")
+}
+
+let greet=(wish,fun)=>{
+    console.log(wish+" this is for you..")
+    fun('Sweet message')
+}
+greet('Alice',quotation)
