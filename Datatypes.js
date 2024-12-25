4deb7673fd8ffb6e62cf5a3a336c64176af3e6e3
PavanKumar355 <pavankumar93477@gmail.com>
2024-12-25 18:23:32 +0530

    added on template literals using backtick ``

--- Datatypes.js
+++ Datatypes.js
@@ -1,3 +1,4 @@
+"use strict";
 //number Data Type: All Numbers (whole, decimal, positive, negative)
 var a = 456/0;
 console.log(a, typeof a);
@@ -17,3 +18,25 @@ console.log(d); //undefined
 //null:
 let e = null;
 console.log(e); //null
+
+
+let name= 'alice';
+console.log(name)
+
+function name1(a,b){
+return a+b;
+}
+console.log(name1(1,1));
+
+let $=56
+console.log($)
+
+//template literals
+var s=`hi "welcome to javascript"`
+var s=`hi 'welcome to javascript'`
+let x='alice'
+var s=`hi '${x}'`
+var s=12
+var s1=13
+var s=`the sum of '${s}' and '${s1}' is: ${s+s1}`
+console.log(s)
